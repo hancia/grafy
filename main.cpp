ead0513c1fc9ee19f5677469aa140865a123af3c
hancia <hanna.klimczak@gmail.com>
2017-04-15 17:19:37 +0200

    Creating list for a graph, no repetition

--- main.cpp
+++ main.cpp
@@ -12,37 +12,45 @@ struct lista
 struct wierzcholek
 {
     int nazwa;
+    int dl;
     lista *sasiad;
 };
 
 void generuj_nast(wierzcholek T[])
 {
     int a,b;
+    lista *current, *prev;
     for(int i=0; i<n; i++)
     {
         T[i].nazwa=i+1;
-        a=rand()%n;
+        for(int m=n; m>=1; m--)
+        {
+            if(m!=i+1)
+            {
+                lista *nowy= new lista;
+                nowy->nazwa=m;
+                if(T[i].dl==0) nowy->next=NULL;
+                else nowy->next=T[i].sasiad;
+                T[i].sasiad=nowy;
+                T[i].dl++;
+            }
+        }
+        a=rand()%T[i].dl;
         for(int m=0; m<a; m++)
         {
-            b=rand()%n+1;
-            if(b!=i)
+            current=T[i].sasiad;
+            b=rand()%T[i].dl;
+            for(int j=0; j<b; j++)
             {
-                if(m==0)
-                {
-                   lista *nowy= new lista;
-                    nowy->nazwa=b;
-                    nowy->next=NULL;
-                    T[i].sasiad=nowy;
-                }
-                else
-                {
-                    lista *nowy= new lista;
-                    nowy->nazwa=b;
-                    nowy->next=T[i].sasiad;
-                    T[i].sasiad=nowy;
-                }
+                prev=current;
+                current=current->next;
             }
-            else b--;
+            if(current==T[i].sasiad)
+            T[i].sasiad=current->next;
+            else
+            prev->next=current->next;
+            T[i].dl--;
+            delete current;
         }
     }
 }
@@ -52,7 +60,7 @@ void wyswietl_nast(wierzcholek T[])
     for(int i=0; i<n; i++)
     {
         lista *obecny=T[i].sasiad;
-        cout<<"wierzcholek "<<T[i].nazwa<<" i lista jego sasiadow: ";
+        cout<<endl<<"wierzcholek "<<T[i].nazwa<<" i lista jego sasiadow: ";
         while(obecny!=NULL)
         {
             cout<<obecny->nazwa<<" ";
@@ -72,23 +80,16 @@ void wpisz_nast(wierzcholek T[])
         T[i].nazwa=i;
         cout<<" ilosc sasiadow: ";
         cin>>a;
-        for(int m=0; m<a; m++)
+        for(int m=1; m<a; m++)
         {
-            if(m==0)
-            {
-               lista *nowy= new lista;
-                nowy->nazwa=i;
-                nowy->next=NULL;
-                //T[i].sasiad.next=nowy;
-                T[i].sasiad=nowy;
-            }
-            else
+            if(m!=i+1)
             {
                 lista *nowy= new lista;
-                nowy->nazwa=i;
-                nowy->next=T[i].sasiad;
-                //T[i].sasiad.next=nowy;
+                nowy->nazwa=m;
+                if(T[i].sasiad==NULL) nowy->next=NULL;
+                else nowy->next=T[i].sasiad;
                 T[i].sasiad=nowy;
+                T[i].dl++;
             }
         }
     }
@@ -110,7 +111,13 @@ void wpisz_nast(wierzcholek T[])
  }
 int main()
 {
+    srand(time(NULL));
     wierzcholek *T = new wierzcholek[n];
+    for(int i=0; i<n; i++)
+    {
+        T[i].sasiad=NULL;
+        T[i].dl=0;
+    }
     generuj_nast(T);
     wyswietl_nast(T);
     usun_nast(T);
