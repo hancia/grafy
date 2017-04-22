8bc2026fdba0728d0d184690e208abd69c24ab81
hancia <hanna.klimczak@gmail.com>
2017-04-22 20:27:16 +0200

    na aisd

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include<ctime>
 #include<cstdlib>
+#include <fstream>
 using namespace std;
-int n=1000;
+int n=4000;
 double g=0.4;
 
+//struktury dla listy nastepnikow
 struct lista
 {
     int nazwa;
@@ -13,12 +15,12 @@ struct lista
 struct wierzcholek
 {
     int nazwa;
-    int dl;
     lista *sasiad;
     bool visited;
     int start, stop;
 };
 
+//struktura dla listy lukow
 struct lista_l
 {
     int w1;
@@ -26,86 +28,62 @@ struct lista_l
     lista_l* next;
 };
 
-void generuj_nast(wierzcholek T[], double gestosc)
+
+//generuje macierz
+void generuj_macierz(int **M, double gestosc,int krawedzie)
 {
-    int a,b,krawedzie=0;
+    gestosc=gestosc*n*(n-1)/2;
+    int a,b;
+    while(krawedzie>=gestosc)
+    {
+        a=rand()%n;
+        b=rand()%n;
+        M[a][b]=0;
+        krawedzie--;
+    }
+}
+
+//generuje liste nastepnikow na podstawie macierzy
+void generuj_nast(wierzcholek T[], int **M)
+{
     lista *current, *prev;
     for(int i=0; i<n; i++)
     {
         T[i].nazwa=i+1;
-        for(int m=n; m>=1; m--)
+        for(int m=0; m<n; m++)
         {
-            if(m!=i+1)
+            if(M[i][m]==1)
             {
                 lista *nowy= new lista;
-                nowy->nazwa=m;
-                if(T[i].dl==0) nowy->next=NULL;
+                nowy->nazwa=m+1;
+                if(T[i].sasiad==NULL) nowy->next=NULL;
                 else nowy->next=T[i].sasiad;
                 T[i].sasiad=nowy;
-                T[i].dl++;
-                krawedzie++;
             }
-        }
-    }
-    gestosc=gestosc*n*(n-1)/2;
-    while(krawedzie>=gestosc)
-    {
-        a=rand()%n;
-        if(T[a].sasiad!=NULL)
-        {
-            current=T[a].sasiad;
-            b=rand()%T[a].dl;
-            for(int j=0; j<b; j++)
-            {
-                prev=current;
-                current=current->next;
-            }
-            if(current==T[a].sasiad)
-            T[a].sasiad=current->next;
-            else
-            prev->next=current->next;
-            T[a].dl--;
-            delete current;
-            krawedzie--;
         }
     }
 }
 
 
-void generuj_macierz(wierzcholek T[], int **M)
+//generuje liste lukow na podstawie listy nastepnikow
+lista_l* generuj_luki(lista_l *pierwszy, int **M)
 {
-    lista *current;
     for(int i=0; i<n; i++)
     {
-        current=T[i].sasiad;
-        while(current!=NULL)
-        {
-            M[i][current->nazwa-1]=1;
-            current=current->next;
-        }
-    }
-}
-
-void generuj_luki(wierzcholek T[], lista_l **pier)
-{
-    lista_l *pierwszy=*pier;
-    lista *current;
-    for(int i=0; i<n; i++)
-    {
-        current=T[i].sasiad;
-        while(current!=NULL)
+        for(int j=0; j<n; j++)
+        if(M[i][j]==1)
         {
             lista_l *nowy= new lista_l;
             nowy->w1=i+1;
-            nowy->w2=current->nazwa;
+            nowy->w2=j+1;
             if(pierwszy==NULL) nowy->next=NULL;
             else nowy->next=pierwszy;
             pierwszy=nowy;
-            current=current->next;
         }
     }
-    *pier=pierwszy;
+    return pierwszy;
 }
+//wyswietla liste nastepnikow
 void wyswietl_nast(wierzcholek T[])
 {
     for(int i=0; i<n; i++)
@@ -120,6 +98,8 @@ void wyswietl_nast(wierzcholek T[])
     }
 }
 
+
+//przechodzi przez liste nastepnikow dfs i oznacza etykiety
 void dfs(wierzcholek T[], int &odwiedzone, lista *stos, int &krok, int E[][2])
 {
     if(stos==NULL)
@@ -158,6 +138,7 @@ void dfs(wierzcholek T[], int &odwiedzone, lista *stos, int &krok, int E[][2])
     dfs(T,odwiedzone,0,krok,E);
 }
 
+//zlicza luki powrotne korzystajac z listy nastepnikow
 int zlicz_luki_nast(wierzcholek T[], int E[][2])
 {
     int luki=0;
@@ -175,6 +156,8 @@ int zlicz_luki_nast(wierzcholek T[], int E[][2])
     return luki;
 }
 
+
+//zlicza luki powrotne korzystajac z macierzy sasiedztwa
 int zlicz_luki_macierz(int **M, int E[][2])
 {
     int luki=0;
@@ -190,9 +173,16 @@ int zlicz_luki_macierz(int **M, int E[][2])
     return luki;
 }
 
+
+//zlicza luki powrotne korzystajac z listy lukow
 int zlicz_luki_l(lista_l *pierwszy, int E[][2])
 {
    int luki=0;
+   /*for(int i=0; i<ilosc_l;i++)
+   {
+       if(E[L[i].w2][0]<E[L[i].w1][0]&&E[L[i].w1][0]<E[L[i].w1][1]&&E[L[i].w1][1]<E[L[i].w2][1])
+        luki++;
+   }*/
    lista_l *current;
    current=pierwszy;
    while(current!=NULL)
@@ -204,6 +194,7 @@ int zlicz_luki_l(lista_l *pierwszy, int E[][2])
    return luki;
 }
 
+//usuwa liste nastepnikow
  void usun_nast(wierzcholek T[])
  {
      lista *current,*current2;
@@ -219,14 +210,15 @@ int zlicz_luki_l(lista_l *pierwszy, int E[][2])
      }
  }
 
+ //usuwa liste lukow
  void usun_luki(lista_l *pierwszy)
  {
      lista_l *current;
      while(pierwszy!=NULL)
      {
-         current=pierwszy->next;
-         delete pierwszy;
-         pierwszy=current;
+         current=pierwszy;
+         pierwszy=pierwszy->next;
+         delete current;
      }
  }
 
@@ -235,6 +227,9 @@ int main()
     srand(time(NULL));
     lista *stos=NULL;
     lista_l *pierwszy=NULL;
+    int krawedzie=0;
+    ofstream wynik;
+    wynik.open("wyniki.txt", std::ofstream::app);
     int odwiedzone=0, krok=0;
     int E[n][2];
     wierzcholek *T = new wierzcholek[n];
@@ -243,27 +238,47 @@ int main()
     {
         M[i]= new int [n];
         for(int j=0; j<n; j++)
-        M[i][j]=0;
+            if(i!=j)
+            {
+                M[i][j]=1;
+                krawedzie++;
+            }
+            else
+            M[i][j]=0;
     }
     for(int i=0; i<n; i++)
     {
         T[i].sasiad=NULL;
-        T[i].dl=0;
         T[i].visited=0;
     }
-    generuj_nast(T,g);
+    generuj_macierz(M,g,krawedzie);
+    generuj_nast(T,M);
+    pierwszy=generuj_luki(pierwszy,M);
+
+
+    clock_t start = clock();
     dfs(T,odwiedzone,stos,krok,E);
-    generuj_macierz(T,M);
-    generuj_luki(T,&pierwszy);
-    cout<<"ilosc lukow dla macierzy "<<zlicz_luki_macierz(M,E);
-    cout<<endl<<"ilosc lukow dla listy nast "<<zlicz_luki_nast(T,E);
-    cout<<endl<<"ilosc lukow dla listy lukow "<<zlicz_luki_l(pierwszy,E);
+    cout<<"ilosc danych "<<n<<" gestosc "<<g<<endl<<"Czas tworzenia etykiet  "<<double(clock()-start)/CLOCKS_PER_SEC;
+
+    cout<<endl<<"Czas zliczania lukow powrotnych dla roznych reprezentacji: "<<endl;
+    double a1,a2,a3;
+    start=clock();
+    zlicz_luki_macierz(M,E);
+    a1=double(clock()-start)/CLOCKS_PER_SEC;
+    start=clock();
+    zlicz_luki_nast(T,E);
+    a2=double(clock()-start)/CLOCKS_PER_SEC;
+    start=clock();
+    zlicz_luki_l(pierwszy,E);
+    a3=double(clock()-start)/CLOCKS_PER_SEC;
+    cout<<"Macierz sasiedztwa "<<a1<<" Lista nastêpnikow "<<a2<<" Lista lukow "<<a3<<endl;
     usun_nast(T);
-    usun_luki(pierwszy);
     delete []T;
     for(int i=0; i<n; i++)
         delete []M[i];
     delete []M;
     M=NULL;
+    wynik.close();
+    usun_luki(pierwszy);
     return 0;
 }
